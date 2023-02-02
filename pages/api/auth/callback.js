7182bad7b5b036895e11ed9d8db054fad4940921
Michael Cheng <michaelnccu@gmail.com>
2023-02-02 14:50:03 +0800

    Update callback.js

--- pages/api/auth/callback.js
+++ pages/api/auth/callback.js
@@ -1,7 +1,7 @@
 // import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-  console.log('hello from callback.js');
+  console.log('hello from callback.js', req);
   const { code } = req.body;
   console.log('code', code);
 }
