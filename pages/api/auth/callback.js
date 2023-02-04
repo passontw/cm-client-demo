2a3257ac3d5bbaeb03e37da62c667b5fb19fdc16
Michael Cheng <michaelnccu@gmail.com>
2023-02-04 13:47:25 +0800

    Update callback.js

--- pages/api/auth/callback.js
+++ pages/api/auth/callback.js
@@ -4,8 +4,8 @@ import { getCookie, setCookie } from 'cookies-next';
 export default async function handler(req, res) {
   console.log('hello from callback.js', req.url);
   const code = req.url.substring(
-    req.url.indexOf('code=') + 1,
-    req.url.indexOf('&'),
+    req.url.lastIndexOf('=') + 1,
+    // req.url.indexOf('&'),
   );
   // const { code } = req.body;
   console.log('code', code);
