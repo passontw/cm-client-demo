d95c176e5229dd1a9719d8fc594d62973984f778
帕斯昂 <passonTomas@linweiqindeMacBook-Pro.local>
2023-03-03 02:08:12 +0800

    feat: social signin 導頁後顯示使用者名稱
    #2

--- pages/api/auth/callback.js
+++ pages/api/auth/callback.js
@@ -1,61 +1,28 @@
-import fetch from 'node-fetch';
-import { setCookie } from 'cookies-next';
+import axios from "axios";
+import { setCookie } from "cookies-next";
+
+const { NEXT_ORY_COOKIE_NAME, NEXT_KRATOS_ADMIN_URL } = process.env;
 
 export default async function handler(req, res) {
-  console.log('hello from callback.js', req.url);
-  const { code } = req.query;
-
-  console.log('code', code);
-  const clientID = process.env.NEXT_PUBLIC_CLIENT_ID;
-  const clientSecret = process.env.NEXT_ORY_CLIENT_SECRET;
-  const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI;
-  console.log("🚀 ~ file: callback.js:15 ~ handler ~ redirectUri:", redirectUri)
-
   try {
-    const params = new URLSearchParams();
-    params.append('code', code);
-    params.append('client_id', clientID);
-    params.append('client_secret', clientSecret);
-    params.append('grant_type', 'authorization_code');
-    params.append('redirect_uri', redirectUri);
-
-    const response = await fetch(
-      process.env.NEXT_PUBLIC_ORY_URL + '/oauth2/token',
-      { method: 'POST', body: params },
+    const cookieString = `${NEXT_ORY_COOKIE_NAME}=${req.cookies[NEXT_ORY_COOKIE_NAME]}`;
+    const { data } = await axios.request(
+      `${NEXT_KRATOS_ADMIN_URL}/api/.ory/sessions/whoami`,
+      {
+        method: "get",
+        headers: {
+          cookie: cookieString,
+        },
+      }
     );
 
-    const data = await response.json();
-    console.log('data', data);
-    if (data.access_token && data.refresh_token) {
-      console.log('prepare to set cookies');
-      setCookie('access_token', data.access_token, { req, res });
-      setCookie('refresh_token', data.refresh_token, { req, res });
-
-      // const a_token = getCookie('access_token', { req, res });
-      // console.log('a_token', a_token);
-      console.log('cookies set');
-      const userinfoResponse = await fetch(
-        process.env.NEXT_PUBLIC_ORY_URL + '/userinfo',
-        {
-          method: 'GET',
-          headers: { Authorization: 'Bearer ' + data.access_token },
-        },
-      );
-      const data2 = await userinfoResponse.json();
-      if (data2.sub) {
-        console.log('data2.sub', data2.sub);
-        setCookie('user_name', data2.sub, { req, res });
-      }
-      // console.log(data2);
-
-      // res.status(200).redirect(307, '/welcome');
-      // return res.status(200).json({
-      //   status: 200,
-      //   data2,
-      // });
+    const email = data?.identity?.traits?.email;
+    if (email) {
+      console.log("email: ", email);
+      setCookie("user_name", email, { req, res });
     }
   } catch (err) {
-    console.log('swapOAuth2Token error:', err);
+    console.log("swapOAuth2Token error:", err);
 
     return res.status(err.response.status).json({
       status: err.response.status,
@@ -63,6 +30,6 @@ export default async function handler(req, res) {
     });
   }
 
-  return res.status(200).redirect(307, '/welcome');
+  return res.status(200).redirect(307, "/welcome");
   // return res.redirect(200, 'http://localhost:3000');
 }
